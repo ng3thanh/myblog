7220b769bc4ef7ccc78db147e0c20b6f8ccc6d62
ng3thanh <ngthanh2093@gmail.com>
2018-01-14 17:12:39 +0700

    update cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        '\App\Console\Commands\GetCoinData',
+//        '\App\Console\Commands\GetCoinData',
     ];
 
     /**
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('coin:get_price')->dailyAt('10:10');
+//         $schedule->command('coin:get_price')->dailyAt('10:10');
     }
 
     /**
