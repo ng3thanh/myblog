260da7d75af1d60933c6530243ba71f529fc7efc
ThanhNB <thanhnb@hblab.vn>
2018-02-09 08:40:12 +0700

    update

--- resources/views/admin/pages/personal/culture/notes/create.blade.php
+++ resources/views/admin/pages/personal/culture/notes/create.blade.php
@@ -9,7 +9,7 @@
     <section class="content">
         <div class="row">
             <div class="col-md-12">
-                <div class="box box-info">
+                <div class="box box-warning">
                     <div class="box-header">
                         <h3 class="box-title">
                             <small>Create new notes</small>
